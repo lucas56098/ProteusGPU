7243cc30f243ab8d16aad8e4ff00216d693a0edc
lucas56098 <lucas@dr-schleuss.de>
2026-05-06 19:05:56 +0200

    Config: this ofc also belongs to the last commit... :D

--- src/global/globals.h
+++ src/global/globals.h
@@ -2,6 +2,84 @@
 #define GLOBALS_H
 #pragma once
 
+// Defaults for Config.sh compile-time constants. Any value set in
+// Config.sh overrides these.
+
+// voronoi compile time settings
+#ifdef dim_2D
+#ifndef _K_
+#define _K_ 35
+#endif
+#ifndef _MAX_P_
+#define _MAX_P_ 30
+#endif
+#ifndef _MAX_T_
+#define _MAX_T_ 60
+#endif
+#ifndef _FAST_K_
+#define _FAST_K_ 15
+#endif
+#ifndef _FAST_MAX_P_
+#define _FAST_MAX_P_ 20
+#endif
+#ifndef _FAST_MAX_T_
+#define _FAST_MAX_T_ 20
+#endif
+#ifndef _FACE_CAPACITY_MULT_
+#define _FACE_CAPACITY_MULT_ 8
+#endif
+#else
+#ifndef _K_
+#define _K_ 190
+#endif
+#ifndef _MAX_P_
+#define _MAX_P_ 50
+#endif
+#ifndef _MAX_T_
+#define _MAX_T_ 96
+#endif
+#ifndef _FAST_K_
+#define _FAST_K_ 35
+#endif
+#ifndef _FAST_MAX_P_
+#define _FAST_MAX_P_ 30
+#endif
+#ifndef _FAST_MAX_T_
+#define _FAST_MAX_T_ 60
+#endif
+#ifndef _FACE_CAPACITY_MULT_
+#define _FACE_CAPACITY_MULT_ 17
+#endif
+#endif
+
+// GPU kernel block sizes
+#ifndef _VORO_BLOCK_SIZE_
+#define _VORO_BLOCK_SIZE_ 64
+#endif
+#ifndef _KNN_BLOCK_SIZE_
+#define _KNN_BLOCK_SIZE_ 256
+#endif
+#ifndef _GRAD_BLOCK_SIZE_
+#define _GRAD_BLOCK_SIZE_ 256
+#endif
+#ifndef _HYDRO_BLOCK_SIZE_
+#define _HYDRO_BLOCK_SIZE_ 256
+#endif
+#ifndef _MESH_BLOCK_SIZE_
+#define _MESH_BLOCK_SIZE_ 256
+#endif
+
+// hydro / mesh
+#ifndef _GAMMA_EOS_
+#define _GAMMA_EOS_ 5./3.
+#endif
+#ifndef _CELL_SHAPING_SPEED_
+#define _CELL_SHAPING_SPEED_ 0.7
+#endif
+#ifndef _CELL_SHAPING_FACTOR_
+#define _CELL_SHAPING_FACTOR_ 0.2
+#endif
+
 // forward declarations for IO types
 class InputHandler;
 struct ICData;
