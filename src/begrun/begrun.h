3f7416669395e9174abae4bfaf3b12ef5e7a26e7
Lucas Schleuss <schleuss@mpa-garching.mpg.de>
2026-09-15 17:39:09 +0200

    [BEGRUN] guard some more illegal flag combinations
    and add the corresponding tests to CI

--- src/begrun/begrun.h
+++ src/begrun/begrun.h
@@ -5,6 +5,15 @@
 #if (!defined(dim_3D) && !defined(dim_2D)) || (defined(dim_3D) && defined(dim_2D))
 #error "Choose a dimension in Config.sh: [dim_3D] OR [dim_2D]"
 #endif
+#if (!defined(CUDA) && !defined(CPU_DEBUG)) || (defined(CUDA) && defined(CPU_DEBUG))
+#error "Choose a backend in Config.sh: [CUDA] OR [CPU_DEBUG]"
+#endif
+#if defined(CUDA_PROFILING) && !defined(CUDA)
+#error "CUDA_PROFILING requires CUDA in Config.sh"
+#endif
+#if defined(CUDA_PROFILING) && !defined(ENABLE_PROFILING)
+#error "CUDA_PROFILING requires ENABLE_PROFILING in Config.sh"
+#endif
 
 namespace begrun {
 
